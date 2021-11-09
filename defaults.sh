612c32167a7a31c3540da1e3aabc7936f58ad728
oliver <contact@oliver.la>
2021-11-09 15:45:01 +0100

    Create defaults.sh

---
+++ defaults.sh
@@ -0,0 +1,54 @@
+#!/usr/bin/env bash
+
+##
+# Screenshots
+##
+defaults write com.apple.screencapture type png
+defaults write com.apple.screencapture save-selections 0
+defaults write com.apple.screencapture show-thumbnail 0
+defaults write com.apple.screencapture showsCursor 1
+defaults write com.apple.screencapture target clipboard
+
+##
+# Fix annoying stuff
+##
+
+# Save to disk (not to iCloud) by default
+defaults write NSGlobalDomain NSDocumentSaveNewDocumentsToCloud -bool false
+
+# Automatically quit printer app once the print jobs complete
+defaults write com.apple.print.PrintingPrefs "Quit When Finished" -bool true
+
+# Disable the “Are you sure you want to open this application?” dialog
+defaults write com.apple.LaunchServices LSQuarantine -bool false
+
+# Disable Resume system-wide
+defaults write NSGlobalDomain NSQuitAlwaysKeepsWindows -bool false
+
+# Reveal IP address, hostname, OS version, etc. when clicking the clock
+# in the login window
+sudo defaults write /Library/Preferences/com.apple.loginwindow AdminHostInfo HostName
+
+# Finder: show all filename extensions
+defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+
+# Finder: allow text selection in Quick Look
+defaults write com.apple.finder QLEnableTextSelection -bool true
+
+# Display full POSIX path as Finder window title
+defaults write com.apple.finder _FXShowPosixPathInTitle -bool true
+
+# When performing a search, search the current folder by default
+defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"
+
+# Avoid creating .DS_Store files on network volumes
+defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
+
+# Show the ~/Library folder
+chflags nohidden ~/Library
+
+# Show the ~/Users folder
+chflags nohidden /Users
+
+# Prevent applications from bouncing in Dock
+defaults write com.apple.dock no-bouncing -bool true
